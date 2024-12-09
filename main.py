293b5544628b6dfdecceccc102adeb9c0f6bde4f
bherych <bogdangerych@gmail.com>
2024-12-09 04:04:16 +0200

    added methods showcases

--- main.py
+++ main.py
@@ -1,19 +1,17 @@
 from classes import BankAccount, Bank
 
 def main():
-    client = BankAccount(1, "John", 1.0)
+    client1 = BankAccount(1, "John", 1.0)
+    client2 = BankAccount(2, "Steve", 55500.0)
+    client3 = BankAccount(3, "Tom", 2300.0)
     bank = Bank()
 
-    client.add_to_balance(5.0)
-    client.show_account()
-    client.substract_from_balance(3.0)
-    client.show_account()
+    bank.add_account(client1)
+    bank.add_account(client2)
+    bank.add_account(client3)
 
-    bank.add_account(client)
-    i = 1
-    for i in range(5):
-        acc = BankAccount(i+1, "Unknown", (i*5.0)**5.0)
-        bank.add_account(acc)
+    bank.show_accounts()
+    bank.sort_accounts_by_balance()
     bank.show_accounts()
 
 main()
